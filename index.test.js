ad7afcbfde59d7c5de06cd4d862fa2a614b363cd
Winifred Lu <winnie.lu@comcast.net>
2022-02-23 15:31:23 -0500

    exercise 7 tests complete

--- index.test.js
+++ index.test.js
@@ -145,6 +145,16 @@ describe('[Exercise 6] Car', () => {
 });
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', () => {
+    utils.isEvenNumberAsync(2)
+      .then(result => {
+        expect(result).toBe(true);
+      })
+  });
+  test('[20] resolves false if passed an odd number', () => {
+    utils.isEvenNumberAsync(3)
+      .then(result => {
+        expect(result).toBe(false);
+      })
+  });
 });
